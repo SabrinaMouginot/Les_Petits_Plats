3626d17ea540d9d542e92f6fcba15b878ee89bb7
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-03-28 09:59:11 +0100

    création de fichiers pour les différents filtres dans un dossier " filtre " pour être plus modulaire , reutilisable , efficace sur la gestion des dépendance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,12 @@
 import RecipeFactory from './recipeFactory.js';
 import RenderFactory from './renderFactory.js';
-import * as FilterFactory from './filterFactory.js';
+import { generateIngredientDropdown } from './filters/ingredientFilter.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     RecipeFactory.loadRecipes()
         .then(recipes => {
             RenderFactory.renderRecipes(recipes);
-            generateIngredientsDropdown(recipes);
+            generateIngredientDropdown(recipes); // Appel de la fonction pour générer le menu déroulant des ingrédients
         })
         .catch(error => console.error('Erreur de chargement des données :', error));
 });
-
-function generateIngredientsDropdown(recipes) {
-    const uniqueIngredients = FilterFactory.getUniqueIngredients(recipes);
-    const ingredientsDropdown = document.querySelector('.btn-ingredients');
-
-    const dropdownMenu = document.createElement('div');
-    dropdownMenu.classList.add('dropdown-menu');
-    uniqueIngredients.forEach(ingredient => {
-        const menuItem = document.createElement('button');
-        menuItem.classList.add('dropdown-item');
-        menuItem.type = 'button';
-        menuItem.textContent = ingredient;
-        menuItem.addEventListener('click', () => {
-            console.log('Ingrédient sélectionné :', ingredient);
-        });
-        dropdownMenu.appendChild(menuItem);
-    });
-
-    ingredientsDropdown.appendChild(dropdownMenu);
-
-    const dropdownArrow = document.querySelector('.dropdown-arrow');
-
-    dropdownArrow.addEventListener('click', function () {
-        dropdownMenu.classList.toggle('show');
-    });
-}
