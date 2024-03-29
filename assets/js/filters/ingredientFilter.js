673ce524a61c3ac7529b72cc17f1130acefe260c
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-03-29 15:06:31 +0100

    les 3 menus déroulants ingrédients , Appareils et Ustensiles présentent tous les choix possibles dans la liste

--- assets/js/filters/ingredientFilter.js
+++ assets/js/filters/ingredientFilter.js
@@ -1,8 +1,38 @@
-import * as FilterFactory from './filterFactory.js';
+// import * as FilterFactory from './filterFactory.js';
 
-// Fonction pour générer le menu déroulant des ingrédients
+// // Fonction pour générer le menu déroulant des ingrédients
+// export function generateIngredientDropdown(recipes) {
+//     const uniqueIngredients = FilterFactory.getUniqueIngredients(recipes);
+//     const ingredientsDropdown = document.querySelector('.btn-ingredients');
+
+//     const dropdownMenu = document.createElement('div');
+//     dropdownMenu.classList.add('dropdown-menu');
+//     uniqueIngredients.forEach(ingredient => {
+//         const menuItem = document.createElement('button');
+//         menuItem.classList.add('dropdown-item');
+//         menuItem.type = 'button';
+//         menuItem.textContent = ingredient;
+//         menuItem.addEventListener('click', () => {
+//             console.log('Ingrédient sélectionné :', ingredient);
+//         });
+//         dropdownMenu.appendChild(menuItem);
+//     });
+
+//     ingredientsDropdown.appendChild(dropdownMenu);
+
+//     const dropdownArrow = document.querySelector('.dropdown-arrow');
+
+//     dropdownArrow.addEventListener('click', function () {
+//         dropdownMenu.classList.toggle('show');
+//     });
+// }
+
+import * as FilterFactory from './filterFactory.js'; // Assurez-vous d'importer FilterFactory
+
 export function generateIngredientDropdown(recipes) {
+    console.log('La fonction generateIngredientDropdown est appelée.');
     const uniqueIngredients = FilterFactory.getUniqueIngredients(recipes);
+    console.log('Ingrédients uniques :', uniqueIngredients);
     const ingredientsDropdown = document.querySelector('.btn-ingredients');
 
     const dropdownMenu = document.createElement('div');
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,55 +2,130 @@ import RecipeFactory from './recipeFactory.js';
 import RenderFactory from './renderFactory.js';
 import { generateIngredientDropdown } from './filters/ingredientFilter.js';
 import { generateApplianceDropdown } from './filters/applianceFilter.js';
-import { generateUstensilDropdown } from './filters/ustensilFilter.js'; // Import de la fonction pour générer le menu déroulant des ustensiles
+import { generateUstensilDropdown } from './filters/ustensilFilter.js';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     RecipeFactory.loadRecipes()
+//         .then(recipes => {
+//             RenderFactory.renderRecipes(recipes);
+//             generateIngredientDropdown(recipes);
+//             generateApplianceDropdown(recipes);
+//             generateUstensilDropdown(recipes);
+
+//             const ingredientsButton = document.querySelector('.btn-ingredients');
+//             const appliancesButton = document.querySelector('.btn-appliance');
+//             const ustensilsButton = document.querySelector('.btn-ustensils');
+
+//             ingredientsButton.addEventListener('click', function () {
+//                 const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
+//                 if (appliancesDropdown.classList.contains('show')) {
+//                     appliancesDropdown.classList.remove('show');
+//                 }
+
+//                 const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
+//                 if (ustensilsDropdown.classList.contains('show')) {
+//                     ustensilsDropdown.classList.remove('show');
+//                 }
+
+//                 const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
+//                 ingredientsDropdown.classList.toggle('show');
+//             });
+
+//             appliancesButton.addEventListener('click', function () {
+//                 const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
+//                 if (ingredientsDropdown.classList.contains('show')) {
+//                     ingredientsDropdown.classList.remove('show');
+//                 }
+
+//                 const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
+//                 if (ustensilsDropdown.classList.contains('show')) {
+//                     ustensilsDropdown.classList.remove('show');
+//                 }
+
+//                 const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
+//                 appliancesDropdown.classList.toggle('show');
+//             });
+
+//             ustensilsButton.addEventListener('click', function () {
+//                 const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
+//                 if (ingredientsDropdown.classList.contains('show')) {
+//                     ingredientsDropdown.classList.remove('show');
+//                 }
+
+//                 const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
+//                 if (appliancesDropdown.classList.contains('show')) {
+//                     appliancesDropdown.classList.remove('show');
+//                 }
+
+//                 const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
+//                 ustensilsDropdown.classList.toggle('show');
+//             });
+//         })
+//         .catch(error => console.error('Erreur de chargement des données :', error));
+// });
 
 
 document.addEventListener('DOMContentLoaded', function () {
     RecipeFactory.loadRecipes()
         .then(recipes => {
             RenderFactory.renderRecipes(recipes);
-            generateIngredientDropdown(recipes); // Appel de la fonction pour générer le menu déroulant des ingrédients
-        })
-        .catch(error => console.error('Erreur de chargement des données :', error));
-});
+            generateIngredientDropdown(recipes);
+            generateApplianceDropdown(recipes);
+            generateUstensilDropdown(recipes);
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    RecipeFactory.loadRecipes()
-        .then(recipes => {
-            RenderFactory.renderRecipes(recipes);
-            generateIngredientDropdown(recipes); // Appel de la fonction pour générer le menu déroulant des ingrédients
-            generateApplianceDropdown(recipes); // Appel de la fonction pour générer le menu déroulant des appareils
-            generateUstensilDropdown(recipes); // Appel de la fonction pour générer le menu déroulant des ustensiles
-
-            // Sélection des boutons d'ingrédients et d'appareils
             const ingredientsButton = document.querySelector('.btn-ingredients');
             const appliancesButton = document.querySelector('.btn-appliance');
+            const ustensilsButton = document.querySelector('.btn-ustensils');
 
-            // Ajout des écouteurs d'événements pour les boutons
             ingredientsButton.addEventListener('click', function () {
-                // Fermer le menu déroulant des appareils s'il est ouvert
                 const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
                 if (appliancesDropdown.classList.contains('show')) {
                     appliancesDropdown.classList.remove('show');
                 }
 
-                // Ouvrir le menu déroulant des ingrédients
+                const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
+                if (ustensilsDropdown.classList.contains('show')) {
+                    ustensilsDropdown.classList.remove('show');
+                }
+
                 const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
                 ingredientsDropdown.classList.toggle('show');
             });
 
             appliancesButton.addEventListener('click', function () {
-                // Fermer le menu déroulant des ingrédients s'il est ouvert
                 const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
                 if (ingredientsDropdown.classList.contains('show')) {
                     ingredientsDropdown.classList.remove('show');
                 }
 
-                // Ouvrir le menu déroulant des appareils
+                const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
+                if (ustensilsDropdown.classList.contains('show')) {
+                    ustensilsDropdown.classList.remove('show');
+                }
+
                 const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
                 appliancesDropdown.classList.toggle('show');
+            });
+
+            ustensilsButton.addEventListener('click', function () {
+                const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
+                if (ingredientsDropdown.classList.contains('show')) {
+                    ingredientsDropdown.classList.remove('show');
+                }
+
+                const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
+                if (appliancesDropdown.classList.contains('show')) {
+                    appliancesDropdown.classList.remove('show');
+                }
+
+                const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
+                ustensilsDropdown.classList.toggle('show');
+            });
+
+            // Ajouter un événement de clic sur le bouton d'ingrédients pour afficher/masquer le menu déroulant
+            ingredientsButton.addEventListener('click', function () {
+                const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
+                ingredientsDropdown.classList.toggle('show');
             });
         })
         .catch(error => console.error('Erreur de chargement des données :', error));
