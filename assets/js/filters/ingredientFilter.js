9d289846b2c8134302da3a49515d75d0861aa385
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-04-08 11:59:59 +0200

    recherche pour filtrer les recettes avec le filtre des ingrédients

--- assets/js/filters/ingredientFilter.js
+++ assets/js/filters/ingredientFilter.js
@@ -1,54 +1,9 @@
-// import * as FilterFactory from './filterFactory.js'; 
-
-// // Déclarer une variable pour stocker l'élément de menu déroulant des ingrédients
-// const ingredientsDropdown = document.querySelector('.btn-ingredients');
-
-// // Fonction pour générer le menu déroulant des ingrédients
-// export function generateIngredientDropdown(recipes) {
-//     console.log('La fonction generateIngredientDropdown est appelée.');
-//     const uniqueIngredients = FilterFactory.getUniqueIngredients(recipes);
-//     console.log('Ingrédients uniques :', uniqueIngredients);
-
-//     const dropdownMenu = document.createElement('div');
-//     dropdownMenu.classList.add('dropdown-menu');
-//     uniqueIngredients.forEach(ingredient => {
-//         const menuItem = document.createElement('button');
-//         menuItem.classList.add('dropdown-item');
-//         menuItem.type = 'button';
-//         menuItem.textContent = ingredient;
-//         menuItem.addEventListener('click', () => {
-//             console.log('Ingrédient sélectionné :', ingredient);
-//             // Ici, vous pouvez appeler une fonction pour traiter la sélection de l'ingrédient
-//             // Par exemple, vous pouvez filtrer les recettes et mettre à jour l'affichage
-//             handleIngredientSelection(ingredient);
-//         });
-//         dropdownMenu.appendChild(menuItem);
-//     });
-
-//     ingredientsDropdown.appendChild(dropdownMenu);
-
-//     const dropdownArrow = document.querySelector('.dropdown-arrow');
-
-//     dropdownArrow.addEventListener('click', function () {
-//         dropdownMenu.classList.toggle('show');
-//     });
-// }
-
-// // Fonction pour gérer la sélection d'ingrédient
-// function handleIngredientSelection(selectedIngredient) {
-//     // Sélection de l'élément où afficher l'ingrédient sélectionné dans le navigateur
-//     const selectedIngredientDisplay = document.querySelector('.selected-ingredient');
-
-//     // Afficher l'ingrédient sélectionné à l'écran
-//     selectedIngredientDisplay.textContent = `${selectedIngredient}`;
-
-// }
-
 import * as FilterFactory from './filterFactory.js';
-import RenderFactory from '../renderFactory.js'; // Assurez-vous que le chemin est correct
 
+// Déclarer une variable pour stocker l'élément de menu déroulant des ingrédients
 const ingredientsDropdown = document.querySelector('.btn-ingredients');
 
+// Fonction pour générer le menu déroulant des ingrédients
 export function generateIngredientDropdown(recipes) {
     console.log('La fonction generateIngredientDropdown est appelée.');
     const uniqueIngredients = FilterFactory.getUniqueIngredients(recipes);
@@ -63,7 +18,10 @@ export function generateIngredientDropdown(recipes) {
         menuItem.textContent = ingredient;
         menuItem.addEventListener('click', () => {
             console.log('Ingrédient sélectionné :', ingredient);
-            handleIngredientSelection(ingredient, recipes); // Passer les recettes comme deuxième argument
+            // Ici,appeler une fonction pour traiter la sélection de l'ingrédient
+            // Par exemple, filtrer les recettes et mettre à jour l'affichage
+            handleIngredientSelection(ingredient);
+            // handleIngredientSelection(ingredient, recipes); // Passer les recettes comme deuxième argument
         });
         dropdownMenu.appendChild(menuItem);
     });
@@ -77,13 +35,23 @@ export function generateIngredientDropdown(recipes) {
     });
 }
 
-function handleIngredientSelection(selectedIngredient, allRecipes) {
+// Fonction pour gérer la sélection d'ingrédient
+function handleIngredientSelection(selectedIngredient) {
+    // Sélection de l'élément où afficher l'ingrédient sélectionné dans le navigateur
     const selectedIngredientDisplay = document.querySelector('.selected-ingredient');
+
+    // Afficher l'ingrédient sélectionné à l'écran
     selectedIngredientDisplay.textContent = `${selectedIngredient}`;
 
-    const filteredRecipes = allRecipes.filter(recipe =>
-        recipe.ingredients.some(ingredient => ingredient.ingredient === selectedIngredient)
-    );
-
-    RenderFactory.renderRecipes(filteredRecipes); // Utiliser la fonction renderRecipes du RenderFactory
 }
+
+// function handleIngredientSelection(selectedIngredient, allRecipes) {
+//     const selectedIngredientDisplay = document.querySelector('.selected-ingredient');
+//     selectedIngredientDisplay.textContent = `${selectedIngredient}`;
+
+//     const filteredRecipes = allRecipes.filter(recipe =>
+//         recipe.ingredients.some(ingredient => ingredient.ingredient === selectedIngredient)
+//     );
+
+//     RenderFactory.renderRecipes(filteredRecipes); // Utiliser la fonction renderRecipes du RenderFactory
+// }
