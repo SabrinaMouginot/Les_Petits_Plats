4ff8f5d37a38a4628ca72c69e4a18033695f5416
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-04-09 14:03:25 +0200

    intégration du fichier renderFactory dans le fichier recipeFactory et suppression du fichier vide renderFactory

--- assets/js/recipeFactory.js
+++ assets/js/recipeFactory.js
@@ -3,8 +3,59 @@ const RecipeFactory = {
     loadRecipes: function () {
         return fetch('assets/json/recipes.json')
             .then(response => response.json());
+    },
+
+    // Fonction pour afficher les recettes sur la page HTML
+    renderRecipes: function (recipes) {
+        const cardContainer = document.querySelector('.card-container');
+        let row = document.createElement('div');
+        row.classList.add('row', 'row-cols-1', 'row-cols-md-3', 'g-4', 'mb-5');
+
+        recipes.forEach((recipe, index) => {
+            if (index % 3 === 0 && index !== 0) {
+                cardContainer.appendChild(row);
+                row = document.createElement('div');
+                row.classList.add('row', 'row-cols-1', 'row-cols-md-3', 'g-4', 'mb-5');
+            }
+
+            const card = document.createElement('div');
+            card.classList.add('col');
+            card.innerHTML = `
+                <div class="card h-100">
+                    <img src="assets/Photos+P7+JS+Les+petits+plats/Photos/${recipe.image}" class="card-img-top img-fluid ImgRecipe" alt="image du plat">
+                    <div class="card-body">
+                        <h5 class="card-title mt-4">${recipe.name}</h5>
+                        <p class="card-text mt-4">RECETTE</p>
+                        <p class="card-text">${recipe.description}</p>
+                        <p class="card-text mt-4">INGREDIENTS</p>
+                        <div class="row">
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    ${recipe.ingredients.slice(0, Math.ceil(recipe.ingredients.length / 2)).map(ingredient => `
+                                        <li  class="fw-bold">${ingredient.ingredient}</li>  
+                                        <li  class="mb-4">${ingredient.quantity !== undefined ? ingredient.quantity : ''} ${ingredient.unit !== undefined ? ingredient.unit : ''}</li>
+                                    `).join('')}
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    ${recipe.ingredients.slice(Math.ceil(recipe.ingredients.length / 2)).map(ingredient => `
+                                        <li class="fw-bold">${ingredient.ingredient}</li>  
+                                        <li class="mb-4">${ingredient.quantity !== undefined ? ingredient.quantity : ''} ${ingredient.unit !== undefined ? ingredient.unit : ''}</li>
+                                    `).join('')}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            row.appendChild(card);
+        });
+
+        cardContainer.appendChild(row);
     }
 };
 
 // Exporter le module pour une utilisation dans d'autres fichiers
 export default RecipeFactory;
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,51 @@
+// // Importer RecipeFactory depuis recipeFactory.js
+// import RecipeFactory from './recipeFactory.js';
+// import { generateIngredientDropdown } from './filters/ingredientFilter.js';
+// import { generateApplianceDropdown } from './filters/applianceFilter.js';
+// import { generateUstensilDropdown } from './filters/ustensilFilter.js';
+
+// // Déclarer la variable globale allRecipes
+// let allRecipes;
+
+// // Écouter l'événement DOMContentLoaded
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Charger les recettes
+//     RecipeFactory.loadRecipes()
+//         .then(recipes => {
+//             // Assurez-vous que les recettes sont accessibles ici
+//             allRecipes = recipes;
+
+//             // Afficher les recettes
+//             RecipeFactory.renderRecipes(recipes);
+//             generateIngredientDropdown(recipes);
+//             generateApplianceDropdown(recipes);
+//             generateUstensilDropdown(recipes);
+
+//             // Sélectionner les boutons
+//             const ingredientsButton = document.querySelector('.btn-ingredients');
+//             const appliancesButton = document.querySelector('.btn-appliance');
+//             const ustensilsButton = document.querySelector('.btn-ustensils');
+
+//             // Ajouter des écouteurs d'événements pour les boutons
+//             ingredientsButton.addEventListener('click', function () {
+//                 const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
+//                 ingredientsDropdown.classList.toggle('show');
+//             });
+
+//             appliancesButton.addEventListener('click', function () {
+//                 const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
+//                 appliancesDropdown.classList.toggle('show');
+//             });
+
+//             ustensilsButton.addEventListener('click', function () {
+//                 const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
+//                 ustensilsDropdown.classList.toggle('show');
+//             });
+//         })
+//         .catch(error => console.error('Erreur de chargement des données :', error));
+// });
+
 import RecipeFactory from './recipeFactory.js';
-import RenderFactory from './renderFactory.js';
 import { generateIngredientDropdown } from './filters/ingredientFilter.js';
 import { generateApplianceDropdown } from './filters/applianceFilter.js';
 import { generateUstensilDropdown } from './filters/ustensilFilter.js';
@@ -11,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // Assurez-vous que les recettes sont accessibles ici
             allRecipes = recipes;
 
-            RenderFactory.renderRecipes(recipes);
+            RecipeFactory.renderRecipes(recipes);
             generateIngredientDropdown(recipes);
             generateApplianceDropdown(recipes);
             generateUstensilDropdown(recipes);
@@ -21,55 +67,40 @@ document.addEventListener('DOMContentLoaded', function () {
             const ustensilsButton = document.querySelector('.btn-ustensils');
 
             ingredientsButton.addEventListener('click', function () {
-                const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
-                if (appliancesDropdown.classList.contains('show')) {
-                    appliancesDropdown.classList.remove('show');
-                }
-
-                const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
-                if (ustensilsDropdown.classList.contains('show')) {
-                    ustensilsDropdown.classList.remove('show');
-                }
-
                 const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
-                ingredientsDropdown.classList.toggle('show');
+                toggleDropdown(ingredientsDropdown);
             });
 
             appliancesButton.addEventListener('click', function () {
-                const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
-                if (ingredientsDropdown.classList.contains('show')) {
-                    ingredientsDropdown.classList.remove('show');
-                }
-
-                const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
-                if (ustensilsDropdown.classList.contains('show')) {
-                    ustensilsDropdown.classList.remove('show');
-                }
-
                 const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
-                appliancesDropdown.classList.toggle('show');
+                toggleDropdown(appliancesDropdown);
             });
 
             ustensilsButton.addEventListener('click', function () {
-                const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
-                if (ingredientsDropdown.classList.contains('show')) {
-                    ingredientsDropdown.classList.remove('show');
-                }
-
-                const appliancesDropdown = document.querySelector('.btn-appliance .dropdown-menu');
-                if (appliancesDropdown.classList.contains('show')) {
-                    appliancesDropdown.classList.remove('show');
-                }
-
                 const ustensilsDropdown = document.querySelector('.btn-ustensils .dropdown-menu');
-                ustensilsDropdown.classList.toggle('show');
+                toggleDropdown(ustensilsDropdown);
             });
 
-            // Ajouter un événement de clic sur le bouton d'ingrédients pour afficher/masquer le menu déroulant
-            ingredientsButton.addEventListener('click', function () {
-                const ingredientsDropdown = document.querySelector('.btn-ingredients .dropdown-menu');
-                ingredientsDropdown.classList.toggle('show');
+            const dropdownArrow = document.querySelector('.dropdown-arrow');
+
+            dropdownArrow.addEventListener('click', function () {
+                const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+                dropdownMenus.forEach(menu => {
+                    if (menu.classList.contains('show')) {
+                        menu.classList.remove('show');
+                    }
+                });
             });
         })
         .catch(error => console.error('Erreur de chargement des données :', error));
 });
+
+function toggleDropdown(dropdown) {
+    const allDropdowns = document.querySelectorAll('.dropdown-menu');
+    allDropdowns.forEach(menu => {
+        if (menu !== dropdown && menu.classList.contains('show')) {
+            menu.classList.remove('show');
+        }
+    });
+    dropdown.classList.toggle('show');
+}
