87c5e726057ad5079deb5242d0218baa434a5d35
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-04-15 15:14:55 +0200

    Si l'appareil existe déjà je le retire du tableau sinon je l ajoute l'appareil au tableau

--- assets/js/filters/applianceFilter.js
+++ assets/js/filters/applianceFilter.js
@@ -7,10 +7,30 @@ export function generateApplianceDropdown(recipes) {
     generateDropdown(uniqueAppliances, '.btn-appliance', handleApplianceSelection);
 }
 
+// export function handleApplianceSelection(selectedAppliance) {
+//     tags.push({
+//         value: selectedAppliance,
+//         type: "appliance"
+//     })
+//     console.log(tags)
+// }
+
 export function handleApplianceSelection(selectedAppliance) {
-    tags.push({
-        value: selectedAppliance,
-        type: "appliance"
-    })
-    console.log(tags)
+    // Vérifier si l'appareil sélectionné existe déjà dans tags
+    const existingIndex = tags.findIndex(tag => tag.value === selectedAppliance && tag.type === "appliance");
+
+    if (existingIndex !== -1) {
+        // Si l'appareil existe déjà, le retirer du tableau
+        tags.splice(existingIndex, 1);
+        console.log(`L'appareil "${selectedAppliance}" a été retiré.`);
+    } else {
+        // Sinon, ajouter l'appareil au tableau
+        tags.push({
+            value: selectedAppliance,
+            type: "appliance"
+        });
+        console.log(`L'appareil "${selectedAppliance}" a été ajouté.`);
+    }
+
+    console.log(tags);
 }
