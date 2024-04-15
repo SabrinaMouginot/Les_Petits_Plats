46b86e71135b46646c91ea882a144d1da78464a0
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-04-15 15:42:31 +0200

    Si l ustensil existe déjà je le retire du tableau sinon je l ajoute l'ustensile au tableau

--- assets/js/filters/ustensilFilter.js
+++ assets/js/filters/ustensilFilter.js
@@ -8,11 +8,22 @@ export function generateUstensilDropdown(recipes) {
     generateDropdown(uniqueUstensils, '.btn-ustensils', handleUstensilSelection);
 }
 
-function handleUstensilSelection(selectedUstensil) {
-    tags.push({
-        value: selectedUstensil,
-        type: "ustensil"
-    })
-    console.log(tags)
-}
+export function handleUstensilSelection(selectedUstensil) {
+    // Vérifier si l'ustensile sélectionné existe déjà dans tags
+    const existingIndex = tags.findIndex(tag => tag.value === selectedUstensil && tag.type === "ustensil");
 
+    if (existingIndex !== -1) {
+        // Si l'ustensile existe déjà, le retirer du tableau
+        tags.splice(existingIndex, 1);
+        console.log(`L'ustensile "${selectedUstensil}" a été retiré.`);
+    } else {
+        // Sinon, ajouter l'ustensile au tableau
+        tags.push({
+            value: selectedUstensil,
+            type: "ustensil"
+        });
+        console.log(`L'ustensile "${selectedUstensil}" a été ajouté.`);
+    }
+
+    console.log(tags);
+}
