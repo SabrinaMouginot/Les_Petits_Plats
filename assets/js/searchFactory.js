6252a28c76bf2ad111ea129587eda9ba5e55117f
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-04-22 13:00:17 +0200

    suppression dun fichier vide

--- assets/js/searchFactory.js
+++
@@ -1,3 +0,0 @@
-// searchFactory.js
-// Ce fichier peut contenir les fonctions pour la recherche de recettes.
-// Pour l'instant, je laisse ce fichier vide, car je n'ai pas encore implémenté cette fonctionnalité.
