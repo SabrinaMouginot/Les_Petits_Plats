efb149f1dc06f0434fffd9e561524246cbe15474
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-04-25 05:44:02 +0200

    tri et affichage des recettes avec les menus deroulants

--- assets/js/filters/applianceFilter.js
+++ assets/js/filters/applianceFilter.js
@@ -29,3 +29,4 @@ export function handleApplianceSelection(selectedAppliance) {
 
     displayTags();
 }
+
--- assets/js/filters/ustensilFilter.js
+++ assets/js/filters/ustensilFilter.js
@@ -30,3 +30,4 @@ export function handleUstensilSelection(selectedUstensil) {
 
     displayTags();
 }
+
